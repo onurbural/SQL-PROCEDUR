cb6884e8b5d6d372b688c3dc13cc596739616f50
onurbural <vwpassat43@outlook.com>
2024-08-07 19:22:24 +0300

    Codes added

---
+++ StoreProcedure/StoreProcedure/Program.cs
@@ -0,0 +1,28 @@
+﻿using StoreProcedure.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace StoreProcedure
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var context = new PROCEDURContext())
+            {
+                try
+                {
+                    var values = await context.Procedures.GetCategoryHierarchyWParams1Async(2);
+                    foreach (var value in values)
+                    {
+                        Console.WriteLine(value.FullPath);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+            }
+        }
+    }
+}
